35a1a045bfd726a038f4607300798d6fd612d025
Isaac Attuah <isaacattuah@gmail.com>
2021-12-15 13:41:04 -0500

    logo asset added and added to login screen

--- lib/Screens/login_page.dart
+++ lib/Screens/login_page.dart
@@ -3,6 +3,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:plant_master_demo/Screens/profile_page.dart';
 import 'package:plant_master_demo/Screens/register_page.dart';
+import 'package:plant_master_demo/Theme/colors.dart';
+import 'package:plant_master_demo/Theme/theme_data.dart';
 import 'package:plant_master_demo/utils/fire_auth.dart';
 import 'package:plant_master_demo/utils/validator.dart';
 
@@ -48,141 +50,158 @@ class _LoginPageState extends State<LoginPage> {
         _focusPassword.unfocus();
       },
       child: Scaffold(
-        appBar: AppBar(
-          title: Text('Plant Master'),
-        ),
-        body: FutureBuilder(
-          future: _initializeFirebase(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return Padding(
-                padding: const EdgeInsets.only(left: 24.0, right: 24.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 24.0),
-                      child: Text(
-                        'Login',
-                        style: Theme.of(context).textTheme.headline1,
-                      ),
-                    ),
-                    Form(
-                      key: _formKey,
-                      child: Column(
-                        children: <Widget>[
-                          TextFormField(
-                            controller: _emailTextController,
-                            focusNode: _focusEmail,
-                            validator: (value) => Validator.validateEmail(
-                              email: value,
-                            ),
-                            decoration: InputDecoration(
-                              hintText: "Email",
-                              errorBorder: UnderlineInputBorder(
-                                borderRadius: BorderRadius.circular(6.0),
-                                borderSide: BorderSide(
-                                  color: Colors.red,
+        body: SafeArea(
+          child: Column(
+            children:
+              [
+                SizedBox(height: 20),
+                Center(child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset('assets/images/logo.png'),
+                )),
+                // Text(
+                //   'Plant Master',
+                //   style: ThemeText.title,
+                // ),
+                Spacer(),
+
+            FutureBuilder(
+              future: _initializeFirebase(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return Padding(
+                    padding: const EdgeInsets.only(left: 24.0, right: 24.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 24.0),
+                          child: Text(
+                            'Login',
+                            //style: Theme.of(context).textTheme.headline2,
+                            style: ThemeText.headerMedium,
+                          ),
+                        ),
+                        Form(
+                          key: _formKey,
+                          child: Column(
+                            children: <Widget>[
+                              TextFormField(
+                                controller: _emailTextController,
+                                focusNode: _focusEmail,
+                                validator: (value) => Validator.validateEmail(
+                                  email: value,
+                                ),
+                                decoration: InputDecoration(
+                                  //labelStyle: ThemeText.body,
+                                  hintText: "Email",
+                                  errorBorder: UnderlineInputBorder(
+                                    borderRadius: BorderRadius.circular(6.0),
+                                    borderSide: BorderSide(
+                                      color: Colors.red,
+                                    ),
+                                  ),
                                 ),
                               ),
-                            ),
-                          ),
-                          SizedBox(height: 8.0),
-                          TextFormField(
-                            controller: _passwordTextController,
-                            focusNode: _focusPassword,
-                            obscureText: true,
-                            validator: (value) => Validator.validatePassword(
-                              password: value,
-                            ),
-                            decoration: InputDecoration(
-                              hintText: "Password",
-                              errorBorder: UnderlineInputBorder(
-                                borderRadius: BorderRadius.circular(6.0),
-                                borderSide: BorderSide(
-                                  color: Colors.red,
+                              SizedBox(height: 8.0),
+                              TextFormField(
+                                controller: _passwordTextController,
+                                focusNode: _focusPassword,
+                                obscureText: true,
+                                validator: (value) => Validator.validatePassword(
+                                  password: value,
+                                ),
+                                decoration: InputDecoration(
+                                  hintText: "Password",
+                                  errorBorder: UnderlineInputBorder(
+                                    borderRadius: BorderRadius.circular(6.0),
+                                    borderSide: BorderSide(
+                                      color: Colors.red,
+                                    ),
+                                  ),
                                 ),
                               ),
-                            ),
-                          ),
-                          SizedBox(height: 24.0),
-                          _isProcessing
-                              ? CircularProgressIndicator()
-                              : Row(
-                            mainAxisAlignment:
-                            MainAxisAlignment.spaceBetween,
-                            children: [
-                              Expanded(
-                                child: ElevatedButton(
-                                  onPressed: () async {
-                                    _focusEmail.unfocus();
-                                    _focusPassword.unfocus();
+                              SizedBox(height: 24.0),
+                              if (_isProcessing) CircularProgressIndicator() else Row(
+                                mainAxisAlignment:
+                                MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Expanded(
+                                    child: ElevatedButton(
+                                      onPressed: () async {
+                                        _focusEmail.unfocus();
+                                        _focusPassword.unfocus();
 
-                                    if (_formKey.currentState!
-                                        .validate()) {
-                                      setState(() {
-                                        _isProcessing = true;
-                                      });
+                                        if (_formKey.currentState!
+                                            .validate()) {
+                                          setState(() {
+                                            _isProcessing = true;
+                                          });
 
-                                      User? user = await FireAuth
-                                          .signInUsingEmailPassword(
-                                        email: _emailTextController.text,
-                                        password:
-                                        _passwordTextController.text,
-                                      );
+                                          User? user = await FireAuth
+                                              .signInUsingEmailPassword(
+                                            email: _emailTextController.text,
+                                            password:
+                                            _passwordTextController.text,
+                                          );
 
-                                      setState(() {
-                                        _isProcessing = false;
-                                      });
+                                          setState(() {
+                                            _isProcessing = false;
+                                          });
 
-                                      if (user != null) {
-                                        Navigator.of(context)
-                                            .pushReplacement(
+                                          if (user != null) {
+                                            Navigator.of(context)
+                                                .pushReplacement(
+                                              MaterialPageRoute(
+                                                builder: (context) =>
+                                                    ProfilePage(user: user),
+                                              ),
+                                            );
+                                          }
+                                        }
+                                      },
+                                      child: Text(
+                                        'Sign In',
+                                        style: TextStyle(color: AppColors().gray),
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(width: 24.0),
+                                  Expanded(
+                                    child: ElevatedButton(
+                                      //style: ButtonStyle(elevation: ),
+                                      onPressed: () {
+                                        Navigator.of(context).push(
                                           MaterialPageRoute(
                                             builder: (context) =>
-                                                ProfilePage(user: user),
+                                                RegisterPage(),
                                           ),
                                         );
-                                      }
-                                    }
-                                  },
-                                  child: Text(
-                                    'Sign In',
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                                ),
-                              ),
-                              SizedBox(width: 24.0),
-                              Expanded(
-                                child: ElevatedButton(
-                                  onPressed: () {
-                                    Navigator.of(context).push(
-                                      MaterialPageRoute(
-                                        builder: (context) =>
-                                            RegisterPage(),
+                                      },
+                                      child: Text(
+                                        'Register',
+                                        style: TextStyle(color: AppColors().gray),
                                       ),
-                                    );
-                                  },
-                                  child: Text(
-                                    'Register',
-                                    style: TextStyle(color: Colors.white),
+                                    ),
                                   ),
-                                ),
-                              ),
+                                ],
+                              )
                             ],
-                          )
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              );
-            }
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                }
 
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          },
+                return Center(child: Image.asset('assets/images/loader.gif', scale: 6,));
+                //   Center(
+                //   child: CircularProgressIndicator(),
+                // );
+              },
+            ),
+                Spacer(flex: 2),
+          ]),
         ),
       ),
     );
